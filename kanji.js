694111ff94ab3899589703bba4188f6cb27b4237
volucrisscr <quinn@volucris.space>
2016-09-04 18:53:49 -0400

    Add display mockup for on/kun reading tabs

--- kanji.js
+++ kanji.js
@@ -79,4 +79,26 @@ function swapClassesOnId(id, oldClass, newClass) {
 		doSubmit();
 	});
 
+
+
+
+
+
 });
+
+// Function for tab display based on a tutorial by W3S - http://www.w3schools.com/howto/howto_js_tabs.asp
+	function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
